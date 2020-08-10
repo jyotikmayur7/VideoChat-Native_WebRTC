fafb5d55010528093de63ccbc93ae9dc197816d2
orangekun9 <heyjyotik@gmail.com>
2020-08-10 10:28:35 +0100

    Added basic code for joining the room and if other user exists in the romm, the current user will get notified with the id of other user and vice versa

--- server.js
+++ server.js
@@ -5,4 +5,23 @@ const server = http.createServer();
 const socket = require('socket.io');
 const io = socket(server);
 
+
+const rooms = {};
+
+io.on('connection', socket => {
+    socket.on('join room', roomId => {
+        if (rooms[roomId]) {
+            rooms[roomId].push(socket.id);
+        } else {
+            romms[roomId] = [socket.id];
+        }
+
+        const otherUser = rooms[roomId].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    })
+})
+
 server.listen(8000, () => console.log('Server is running on port 8000'));
